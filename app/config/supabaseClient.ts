5019cce841892e9212f217ce4b7bc3cf40375172
D2G8 <digo83389@gmail.com>
2025-10-29 09:31:55 +0000

    tentativa 111

--- app/config/supabaseClient.ts
+++ app/config/supabaseClient.ts
@@ -1,7 +1,7 @@
 // lib/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://pzhlayhkmqsfewgwynir.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6aGxheWhrbXFzZmV3Z3d5bmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NTI5MTAsImV4cCI6MjA3NjIyODkxMH0.E9BF5yz48Kh3FLIn6nvSNzWL23Mh6hR7hX6Rz21lxiA'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  'https://pzhlayhkmqsfewgwynir.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6aGxheWhrbXFzZmV3Z3d5bmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA2NTI5MTAsImV4cCI6MjA3NjIyODkxMH0.E9BF5yz48Kh3FLIn6nvSNzWL23Mh6hR7hX6Rz21lxiA'
+)
